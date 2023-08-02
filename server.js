c976fc41f10735fc5bb642efb2b4b11e29f99990
Sahadeva Gowda <sahadevamk121@gmail.com>
2023-08-02 23:23:35 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+// FOR SERVER
+// in order to build a server we have to do 2 things
+// * first crearte server
+// * and start the server
+
+const http = require('http')
+const url = require('url')
+const server = http.createServer((req,res)=>{
+    const pathName = req.url
+    console.log(pathName)
+    if (pathName === '/' || pathName === '/overview'){
+        res.end('you are in overview page')
+        console.log('server stopped')
+    }else if(pathName === '/products'){
+        res.end('you are in product page')
+    } else{
+    res.end('hello from server')
+    }
+});
+
+server.listen(8000,'127.0.0.1',() =>{
+    console.log("listen at port no 8000")
+})
